6864cc0ff2ac353bb5f3625333d09f502469e267
Jinseo Park <jinseo@google.com>
2020-05-21 15:43:01 -0400

    Remove deprecated code

--- src/pages/audification-experiment/audification-experiment.component.ts
+++ src/pages/audification-experiment/audification-experiment.component.ts
@@ -1,22 +1,8 @@
-import { AfterViewChecked, Component, ElementRef } from '@angular/core';
-import { AudificationD3 } from '../../d3/experiments/audification.d3';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-audification-experiment',
   templateUrl: './audification-experiment.component.html',
 })
-export class AudificationExperimentComponent implements AfterViewChecked {
-  private audificationD3: AudificationD3;
-
-  constructor(element: ElementRef) {
-    this.audificationD3 = new AudificationD3(element);
-  }
-
-  ngAfterViewChecked() {
-    this.audificationD3.apply({
-      duration: 0,
-      pitchRange: [0, 0],
-    });
-  }
-
+export class AudificationExperimentComponent {
 }
