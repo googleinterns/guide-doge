700c7b091441113bc8bec223f8259b1b304b9c60
Jinseo Park <jinseo@google.com>
2020-06-04 18:07:53 -0400

    Update app.component.spec.ts

--- src/app.component.spec.ts
+++ src/app.component.spec.ts
@@ -20,9 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'guide-doge'`, () => {
+  it(`should have as title 'Guide-Doge'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('guide-doge');
+    expect(app.title).toEqual('Guide-Doge');
   });
 });
