f0c2247dc5d2d382b5e4a858a9234f2527829905
Carlos Villapudua <cvillapudua@google.com>
2020-06-11 02:38:09 +0000

    need to fix spheres (all populate in same place)

--- src/vrdevtest/index.js
+++ src/vrdevtest/index.js
@@ -10,7 +10,6 @@ class Scatter{
     // create a scale so that there is correspondence between data set and screen render
     var pt = new dataPt("a-sphere", data);
     pt.generatePts();  
-    //console.log("hello");
   }
 }
 
@@ -18,10 +17,8 @@ class dataPt{
    constructor(shape, data){
      this.shape = shape;
      this.data = data;
-     var scene = d3.select("a-scene");
-     this.scene = scene;
-     var ptUpdate = scene.selectAll(this.shape).data(this.data);
-     this.ptUpdate = ptUpdate;
+     this.scene = d3.select("a-scene");
+     this.ptUpdate = scene.selectAll(this.shape).data(this.data);
    }
   generatePts(){
      var hscale = d3.scaleLinear();
@@ -31,22 +28,13 @@ class dataPt{
      //enter identifies any DOM elements to be added when # array elements doesn't match
     var dataPt = this.scene.selectAll(this.shape).data(this.data)
     dataPt.enter().append(this.shape);
-    console.log(this.data)
      
-    dataPt.attr("position", (d,i) => {                         //attr("position", (d,i) => { IS CORRECT FUNCTION NOTATION
+    this.ptUpdate.attr("position" = (d,i) => {
       var x = 0;
       var y = i*5;
       var z = -d/10;
       return (x + " " + y + " " + z);
     });  
-    // dataPt.attr({
-    //   position: function (d, i){
-    //     var x = 0;
-    //     var y = i*5;
-    //     var z = -d/10;
-    //     return (x + " " + y + " " + z);
-    //   }
-    // })
   }
 }
   
