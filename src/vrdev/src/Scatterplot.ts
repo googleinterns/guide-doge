ea0e2235330a7c9a10666539c643badc41e74a9f
Carlos Villapudua <cvillapudua@google.com>
2020-06-16 01:29:11 +0000

    moved dataPt functions to Scatter class

--- src/vrdev/src/Scatterplot.ts
+++ src/vrdev/src/Scatterplot.ts
@@ -4,57 +4,45 @@ declare global {
   const d3: typeof _d3;
 }
 
-class Scatter{
-    x: number;
-    y: number;
-    z: number;
+class Scatterplot{
+    data: number[];
+    shape: string;
 
   constructor(){
-    this.x = 2;
-    this.y = 2;
-    this.z = 2;
-    
+    this.data = [0, 0];
   }
   init(){
-    let data: number[] = [10,20,30,40,50,60,70,80];
-
     // create a scale so that there is correspondence between data set and screen render
-    let pts = new dataPt("a-sphere", data);
-    pts.generatePts();  
-    pts.setColor("blue");
+    this.data = [10,20,30,40,50,60,70,80];
+    this.generatePts("a-sphere");  
+    this.setColor("blue");
     //console.log("hello");
   }
-}
-
-class dataPt{
-    shape: string;
-    data: number[];
-
-   constructor(shape: string, data: number[]){
-     this.shape = shape;
-     this.data = data;
-   }
-  generatePts(){
-    var hscale = d3.scaleLinear();
+  generatePts(shape: string){
+    this.shape = shape;
+    let hscale = d3.scaleLinear();
     hscale.domain([0, d3.max(this.data) as number])       //max of dataset
     .range([0, 10]);                                      //linear mapping of data set values to values from 0 to 10
 
      //enter identifies any DOM elements to be added when # array elements doesn't match
-    var datPt = d3.select("a-scene").selectAll(this.shape).data(this.data).enter().append(this.shape);
+    let datPt = d3.select("a-scene").selectAll(shape).data(this.data).enter().append(shape);
 
     //select all shapes within scene 
     //d is data at index, i within 
-    d3.selectAll(this.shape).attr("position", (d, i) => {
-      var x = 0;
-      var y = i*5;
-      var z = -this.data[i];
+    d3.selectAll(shape).attr("position", (d, i) => {
+      let x = 0;
+      let y = i*5;
+      let z = -this.data[i];
       return (x + " " + y + " "   + z);
     });
   }
+  //generatePts must be called before set color, bc then this.shape is undefined
   setColor(color){
     d3.selectAll(this.shape).attr("color", () => {
       return color;
     })
   }
 }
+
+
   
---
+++ src/vrdev/tests/test.spec.ts
@@ -0,0 +1,13 @@
+import Calculator from '../src/Calculator';
+describe('calculate', function() {
+    it('add', function() {
+      let result = Calculator.Sum(5, 2);
+      expect(result).toBe(7);
+    });
+  });
+  describe('hellohello', function() {
+    it('add', function() {
+      let result = Calculator.Sum(5, 2);
+      expect(result).toBe(7);
+    });
+  });
