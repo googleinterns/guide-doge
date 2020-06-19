cb74b853ebf484bc906d8f2b7e25ed917a52a8bc
Jinseo Park <jinseo@google.com>
2020-06-19 17:12:27 -0400

    Add tests for d3

---
+++ src/d3/line-chart.d3.spec.ts
@@ -0,0 +1,81 @@
+import { ElementRef } from '@angular/core';
+import { Datum } from './xy-chart.d3';
+import { Subject } from 'rxjs';
+import { LineChartD3 } from './line-chart.d3';
+
+describe('LineChartD3', () => {
+  const svgElement = document.createElement('svg');
+  const containerElement = document.createElement('div');
+  containerElement.appendChild(svgElement);
+  const renderOptions = {
+    elementRef: new ElementRef<HTMLElement>(containerElement),
+    height: 256,
+    width: 256,
+    marginTop: 8,
+    marginRight: 8,
+    marginBottom: 8,
+    marginLeft: 8,
+    data$: new Subject<Datum[]>(),
+    activeDatum$: new Subject<Datum | null>(),
+  };
+  let lineChartD3: LineChartD3;
+  const transitionDelay = 500;
+  const mockDatum = { date: new Date(), value: 0 };
+
+  beforeEach(() => {
+    lineChartD3 = new LineChartD3(renderOptions);
+  });
+
+  afterEach(() => {
+    lineChartD3.clear();
+  });
+
+  it('should instantiate.', () => {
+    expect(lineChartD3).toBeInstanceOf(LineChartD3);
+  });
+
+  it('should render a path element for the data.', () => {
+    lineChartD3.render();
+    const pathElement = svgElement.querySelector('path');
+    expect(pathElement).toBeTruthy();
+  });
+
+  it('should update the path element upon the data change.', async () => {
+    lineChartD3.render();
+    const pathElement = svgElement.querySelector('path')!;
+    const dAttribute = pathElement.getAttribute('d');
+    renderOptions.data$.next([mockDatum]);
+    await new Promise(resolve => setTimeout(resolve, transitionDelay));
+    const newDAttribute = pathElement.getAttribute('d');
+    expect(newDAttribute !== dAttribute).toBeTrue();
+  });
+
+  it('should render a circle element for the active datum.', async () => {
+    lineChartD3.render();
+    const circleElement = svgElement.querySelector('circle');
+    expect(circleElement).toBeTruthy();
+  });
+
+  it('should update the circle element upon the active datum changes.', async () => {
+    lineChartD3.render();
+    const circleElement = svgElement.querySelector('circle')!;
+    const transformAttribute = circleElement.getAttribute('transform');
+    renderOptions.activeDatum$.next(mockDatum);
+    await new Promise(resolve => setTimeout(resolve, transitionDelay));
+    const newTransformAttribute = circleElement.getAttribute('transform');
+    expect(newTransformAttribute !== transformAttribute).toBeTrue();
+  });
+
+  it('should only show the circle element when the active datum is non-null.', async () => {
+    lineChartD3.render();
+    const circleElement = svgElement.querySelector('circle')!;
+
+    renderOptions.activeDatum$.next(mockDatum);
+    await new Promise(resolve => setTimeout(resolve, transitionDelay));
+    expect(circleElement.getAttribute('display')).toBe('inherit');
+
+    renderOptions.activeDatum$.next(null);
+    await new Promise(resolve => setTimeout(resolve, transitionDelay));
+    expect(circleElement.getAttribute('display')).toBe('none');
+  });
+});
