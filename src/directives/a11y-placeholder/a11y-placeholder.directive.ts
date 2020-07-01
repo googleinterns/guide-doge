37bd5eed76a5a0fa8e0970b1dc5703ebe3c92d6e
Jinseo Park <jinseo@google.com>
2020-06-30 22:50:25 -0400

    Don't use parent injector when compiling module

--- src/directives/a11y-placeholder/a11y-placeholder.directive.ts
+++ src/directives/a11y-placeholder/a11y-placeholder.directive.ts
@@ -10,15 +10,18 @@ export class A11yPlaceholderDirective<Host> {
     private componentFactoryResolver: ComponentFactoryResolver,
     private viewContainerRef: ViewContainerRef,
     private compiler: Compiler,
-    private injector: Injector,
   ) {
   }
 
   async addComponent<Component>(A11yModule: Type<LazyA11yModule<Component>>, host: Host, preference: Preference) {
     this.viewContainerRef.clear();
+
+    // compile the asynchronously imported module to resolve the dependencies of the a11y component
     const moduleFactory = await this.compiler.compileModuleAsync(A11yModule);
-    const moduleRef = moduleFactory.create(this.injector);
+    const moduleRef = moduleFactory.create(null);
     const module = moduleRef.instance;
+
+    // create a component, providing the host to be injected
     const injector = Injector.create({
       providers: [{
         provide: 'host',
