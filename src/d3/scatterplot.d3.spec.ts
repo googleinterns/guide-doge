960622870949fbd5a7a27bb76113d91830725856
Carlos Villapudua <cvillapudua@google.com>
2020-07-20 23:31:46 +0000

    added newer testing file -not done w/ position tst

--- src/d3/scatterplot.d3.spec.ts
+++ src/d3/scatterplot.d3.spec.ts
@@ -1,43 +1,62 @@
 import { Scatterplot } from './scatterplot.d3';
-import { resolve } from 'dns';
+import { TimeSeriesPoint } from '../datasets/queries/time-series.query';
+import { VRTimeSeriesPoint } from '../datasets/queries/vr-time-series.query';
+
+const shape = 'a-sphere';
+let element: HTMLElement;
+let scatterplot: Scatterplot;
+let startDate: Date = new Date();
 
 describe('VR Scatter Plot', () => {
-  const shape = 'a-sphere';
-  let element: HTMLElement;
-  let scatterplot: Scatterplot;
+  let lineChartData1: TimeSeriesPoint[]  = [];
+  lineChartData1.push({x: new Date(), y: 10});
+
+  let scatterPlotData1: VRTimeSeriesPoint[]  = [];
+  scatterPlotData1.push({x: new Date(), y: 10, z: 10});
+
+  let lineChartData8: TimeSeriesPoint[] = [];
+  for (let i = 0; i < 7; i++){
+    lineChartData8.push({x: new Date(), y: i * 10});
+  }
+  
+  let scatterPlotData8: VRTimeSeriesPoint[] = [];
+  for (let i = 0; i < 7; i++){
+    scatterPlotData8.push({x: new Date(), y: i * 10, z: i * 5});
+  }
 
   beforeEach( () =>  {
     element = document.createElement('a-scene');
     scatterplot = new Scatterplot(shape);
+
   });
   it('places no points bc 1:1 correspondence with empty element array', () => {
-    scatterplot.init(element, []);
+    scatterplot.init(element, [], 'line');
     const expectedPosArray = [];
     const result = getPosition(element, shape);
     expect(result).toEqual(expectedPosArray);
   });
-  it('places points for each element in a one element array', () => {
-    scatterplot.init(element, [10]);
-    const expectedPosArray = [{ x: 0, y: 0, z: -20 }];
-    const result = getPosition(element, shape);
-    expect(result).toEqual(expectedPosArray);
-  });
-  it('places points for each element in a two element array', () => {
-    scatterplot.init(element, [10, 10]);
-    const expectedPosArray = [{ x: 0, y: 0, z: -20 }, { x: 5, y: 10, z: -20 }];
-    const result = getPosition(element, shape);
-    expect(result).toEqual(expectedPosArray);
-  });
-  it('places points for each element in a eight element array', () => {
-    scatterplot.init(element, [10, 10, 20, 20, 30, 30, 40, 40]);
-    const expectedPosArray = [
-      { x: 0, y: 0, z: -20 }, { x: 5, y: 10, z: -20 },
-      { x: 10, y: 20, z: -40 }, { x: 15, y: 30, z: -40 },
-      { x: 20, y: 40, z: -60 }, { x: 25, y: 50, z: -60 },
-     { x: 30, y: 60, z: -80 }, { x: 35, y: 70, z: -80 }];
-    const result = getPosition(element, shape);
-    expect(result).toEqual(expectedPosArray);
-  });
+  // it('places points for each element in a one element array', () => {
+  //   scatterplot.init(element, test1);
+  //   const expectedPosArray = [{ x: 0, y: 0, z: -20 }];
+  //   const result = getPosition(element, shape);
+  //   expect(result).toEqual(expectedPosArray);
+  // });
+  // it('places points for each element in a two element array', () => {
+  //   scatterplot.init(element, [10, 10]);
+  //   const expectedPosArray = [{ x: 0, y: 0, z: -20 }, { x: 5, y: 10, z: -20 }];
+  //   const result = getPosition(element, shape);
+  //   expect(result).toEqual(expectedPosArray);
+  // });
+  // it('places points for each element in a eight element array', () => {
+  //   scatterplot.init(element, [10, 10, 20, 20, 30, 30, 40, 40]);
+  //   const expectedPosArray = [
+  //     { x: 0, y: 0, z: -20 }, { x: 5, y: 10, z: -20 },
+  //     { x: 10, y: 20, z: -40 }, { x: 15, y: 30, z: -40 },
+  //     { x: 20, y: 40, z: -60 }, { x: 25, y: 50, z: -60 },
+  //    { x: 30, y: 60, z: -80 }, { x: 35, y: 70, z: -80 }];
+  //   const result = getPosition(element, shape);
+  //   expect(result).toEqual(expectedPosArray);
+  // });
 });
 
 describe('Adding VR Grids/Sky', () => {
