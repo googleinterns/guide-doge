f59b4fee81f0a5bef59434c75ce3319382c5dcdb
Chunnien Chan <cnchan@google.com>
2020-07-20 22:02:23 -0700

    Add tests for WorkdayHoliday datasets

---
+++ src/datasets/user-workday-holiday.dataset.spec.ts
@@ -0,0 +1,48 @@
+import * as UserWorkdayHolidayDataset from './user-workday-holiday.dataset';
+import { Dataset } from './types';
+import { createDefault } from '../utils/preferences';
+import { DAY } from '../utils/timeUnits';
+import { DataCube } from '../models/data-cube/data-cube.model';
+import { Meta } from './metas/types';
+
+describe('UserWorkdayHolidayDataset', () => {
+  let dataset: Dataset;
+  let currentTime: number;
+
+  beforeEach(() => {
+    const config = createDefault(UserWorkdayHolidayDataset.configMeta);
+    dataset = UserWorkdayHolidayDataset.create(config);
+    currentTime = Date.now();
+  });
+
+  it('should contain dataCube.', () => {
+    expect(dataset.dataCube).toBeInstanceOf(DataCube);
+  });
+
+  it('should query data points in date range.', () => {
+    const testMeta = (meta: Meta) => {
+      if (meta.type === 'tabbed') {
+        meta.metas.map(testMeta);
+      } else {
+        const testRanges: [Date, Date][] = [
+          [new Date(currentTime - 10 * DAY), new Date(currentTime - 5 * DAY)],
+          [new Date(currentTime + 5 * DAY), new Date(currentTime + 10 * DAY)],
+          [new Date(currentTime), new Date(currentTime)],
+          [new Date(currentTime + 1), new Date(currentTime - 1)]
+        ];
+
+        for (const range of testRanges) {
+          const data = meta.queryData({ range });
+          for (const datum of data) {
+            for (const point of datum.points) {
+              expect(point.x > range[0]).toBeTrue();
+              expect(point.x <= range[1]).toBeTrue();
+            }
+          }
+        }
+      }
+    };
+
+    dataset.metas.map(testMeta);
+  });
+});
