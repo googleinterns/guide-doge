ead3759d6c82130a61a35f5a91dd1916832f2039
Jinseo Park <jinseo@google.com>
2020-07-21 20:26:41 -0400

    Update tests

--- src/datasets/geographic.dataset.spec.ts
+++ src/datasets/geographic.dataset.spec.ts
@@ -1,6 +1,7 @@
 import * as GeographicDataset from './geographic.dataset';
 import { Dataset } from './types';
 import { createDefault } from '../utils/preferences';
+import { MetaType } from './metas/types';
 
 describe('GeographicDataset', () => {
   let dataset: Dataset;
@@ -11,6 +12,6 @@ describe('GeographicDataset', () => {
   });
 
   it('should contain at least one geo-map meta.', () => {
-    expect(dataset.metas.some(meta => meta.type === 'geo-map')).toBeTrue();
+    expect(dataset.metas.some(meta => meta.type === MetaType.GEO_MAP)).toBeTrue();
   });
 });
