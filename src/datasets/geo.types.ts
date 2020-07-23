24c7437d066729c13e51ba54dbf1ce61cf79487b
Jinseo Park <jinseo@google.com>
2020-07-22 22:24:44 -0400

    Separate raw types

--- src/datasets/geo.types.ts
+++ src/datasets/geo.types.ts
@@ -1,42 +1,61 @@
 import { GeometryCollection, MultiPolygon, Polygon, Topology } from 'topojson-specification';
 
-export interface City {
+interface RawCity {
   id: string;
-  countryId: string;
-  subcontinentId: string;
-  continentId: string;
   name: string;
   lat: number;
   lng: number;
   population: number;
 }
 
-export interface Country {
+interface RawCountry {
   id: string;
-  subcontinentId: string;
-  continentId: string;
   name: string;
-  cities: Record<string, City>;
+  cities: Record<string, RawCity>;
   geometry?: Polygon | MultiPolygon;
 }
 
-export interface Subcontinent {
+interface RawSubcontinent {
   id: string;
-  continentId: string;
   name: string;
+  countries: Record<string, RawCountry>;
+}
+
+interface RawContinent {
+  id: string;
+  name: string;
+  subcontinents: Record<string, RawSubcontinent>;
+}
+
+interface RawWorld {
+  continents: Record<string, RawContinent>;
+  topology: Topology<{ land: GeometryCollection }>;
+}
+
+export interface City extends RawCity {
+  countryId: string;
+  subcontinentId: string;
+  continentId: string;
+}
+
+export interface Country extends RawCountry {
+  subcontinentId: string;
+  continentId: string;
+  cities: Record<string, City>;
+}
+
+export interface Subcontinent extends RawSubcontinent {
+  continentId: string;
   countries: Record<string, Country>;
 }
 
-export interface Continent {
-  id: string;
-  name: string;
+export interface Continent extends RawContinent {
   subcontinents: Record<string, Subcontinent>;
 }
 
-export interface World {
+export interface World extends RawWorld {
   continents: Record<string, Continent>;
   subcontinents: Record<string, Subcontinent>;
   countries: Record<string, Country>;
   cities: Record<string, City>;
-  topology: Topology<{ land: GeometryCollection }>;
 }
