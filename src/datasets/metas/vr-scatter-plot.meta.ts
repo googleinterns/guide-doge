d11c6b8afdf8aa7403e3c19d23903e0572bf962b
Carlos Villapudua <cvillapudua@google.com>
2020-07-23 01:51:28 +0000

    changing legend style on scatterplot meta

--- src/datasets/metas/vr-scatter-plot.meta.ts
+++ src/datasets/metas/vr-scatter-plot.meta.ts
@@ -1,12 +1,12 @@
 import { VRTimeSeriesQuery } from '../queries/vr-time-series.query';
-import { LegendItemStyle as LineChartLegendItemStyle } from '../../d3/line-chart.d3';
+import { ScatterPlotStyle as ScatterPlotLegendItemStyle } from '../../d3/scatterplot.d3';
 import { XYZChartMeta } from './types';
 
-export type VRScatterplotMeta = XYZChartMeta<'vrScatter', VRTimeSeriesQuery<LineChartLegendItemStyle>>;
+export type VRScatterplotMeta = XYZChartMeta<'vrScatter', VRTimeSeriesQuery<ScatterPlotLegendItemStyle>>;
 
 export function createVRScatterplotMeta(
   title: string,
-  queryData: VRTimeSeriesQuery<LineChartLegendItemStyle>,
+  queryData: VRTimeSeriesQuery<ScatterPlotLegendItemStyle>,
 ): VRScatterplotMeta {
   return {
     type: 'vrScatter',
