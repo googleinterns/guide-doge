4f467210f45f9cc28d16849d233ffc0a2c4f53fc
HarrisonPearl <Hspearl@gmail.com>
2020-07-24 17:28:49 -0400

    reformats testing for typescript best practices

--- src/d3/hapticplot.d3.spec.ts
+++ src/d3/hapticplot.d3.spec.ts
@@ -1,86 +1,83 @@
 import { Hapticplot } from './hapticplot.d3';
+import { Entity } from 'aframe';
 
 
 describe('VR Haptic Plot', () => {
   const shape = 'a-sphere';
-  let element: HTMLElement;
+  let scene: HTMLElement;
   let hapticplot: Hapticplot;
 
   beforeEach( () =>  {
-    element = document.createElement('a-scene');
+    scene = document.createElement('a-scene');
     hapticplot = new Hapticplot(shape);
   });
 
-  it('places no points bc 1:1 correspondence with empty element array', () => {
-    hapticplot.init(element, []);
+  it('places no points bc 1:1 correspondence with empty data array', () => {
+    hapticplot.init(scene, []);
     const expectedPosArray = [];
-    const result = getPosition(element, shape);
+    const result = getPosition(scene, shape);
     expect(result).toEqual(expectedPosArray);
   });
 
-  it('places points for each element in a one element array', () => {
-    hapticplot.init(element, [10]);
+  it('places points for each datum in a one datum array', () => {
+    hapticplot.init(scene, [10]);
     const expectedPosArray = [{ x: 0, y: 10, z: -1 }];
-    const result = getPosition(element, shape);
+    const result = getPosition(scene, shape);
     expect(result).toEqual(expectedPosArray);
   });
 
-  it('places points for each element in a two element array', () => {
-    hapticplot.init(element, [10, 10]);
+  it('places points for each datum in a two datum array', () => {
+    hapticplot.init(scene, [10, 10]);
     const expectedPosArray = [{ x: 0, y: 10, z: -1 }, { x: 0.1, y: 10, z: -1 }];
-    const result = getPosition(element, shape);
+    const result = getPosition(scene, shape);
     expect(result).toEqual(expectedPosArray);
   });
 
-  it('places points for each element in a eight element array', () => {
-    hapticplot.init(element, [10, 10, 20, 20, 30, 30, 40, 40]);
+  it('places points for each datum in a eight datum array', () => {
+    hapticplot.init(scene, [10, 10, 20, 20, 30, 30, 40, 40]);
     const expectedPosArray = [
       { x: 0, y: 10, z: -1 }, { x: 0.10, y: 10, z: -1 },
       { x: 0.2, y: 20, z: -1 }, { x: 0.3, y: 20, z: -1 },
       { x: 0.4, y: 30, z: -1 }, { x: 0.5, y: 30, z: -1 },
      { x: 0.6, y: 40, z: -1 }, { x: 0.7, y: 40, z: -1 }];
-    const result = getPosition(element, shape);
+    const result = getPosition(scene, shape);
     expect(result).toEqual(expectedPosArray);
   });
 
-  it('places points for each element in a three element array, and checks their color property', () => {
-    hapticplot.init(element, [10, 20, 30]);
+  it('places points for each datum and sets the correct color property on the resulting shape entities', () => {
+    hapticplot.init(scene, [10, 20, 30]);
     const expectedColorArray = ['green', 'green' , 'green'];
-    const result = getColor(element, shape);
+    const result = getColor(scene, shape);
     expect(result).toEqual(expectedColorArray);
   });
 
-  it('places points for each element in a three element array, and checks their size property', () => {
-    hapticplot.init(element, [10, 20, 30]);
+  it('places points for each datum and sets the correct size property on the resulting shape entities', () => {
+    hapticplot.init(scene, [10, 20, 30]);
     const expectedSizeArray = ['0.05', '0.05', '0.05'];
-    const result = getRadius(element, shape);
+    const result = getRadius(scene, shape);
     expect(result).toEqual(expectedSizeArray);
   });
 
-  it('places points for each element in a three element array, and checks their hoverable property', () => {
-    hapticplot.init(element, [10, 20, 30]);
+  it('places points for each datum and sets the correct hoverable property on the resulting shape entities', () => {
+    hapticplot.init(scene, [10, 20, 30]);
     const expectedSizeArray = ['hoverable', 'hoverable', 'hoverable'];
-    const result = getHoverable(element, shape);
+    const result = getHoverable(scene, shape);
     expect(result).toEqual(expectedSizeArray);
   });
 
-  it('places points for each element in a three element array, and checks their color after a hover-start event', () => {
-    hapticplot.init(element, [10, 20, 30]);
+  it('places points for each datum and sets the correct color property on the resulting shape entities after a hover event', () => {
+    hapticplot.init(scene, [10, 20, 30]);
     const expectedSizeArray = ['red', 'red', 'red'];
-    const result = getHoveredColor(element, shape);
+    const result = getHoveredColor(scene, shape);
     expect(result).toEqual(expectedSizeArray);
   });
 });
 
-/*
-*
-Helper Functions
-*
-*/
+// Helper Functions
 
 // returns array of actual position vectors
-function getPosition(element: HTMLElement, shape: string): Array<{x: number, y: number, z: number}>{
-  const childrenArray = element.querySelectorAll(shape);
+function getPosition(scene: HTMLElement, shape: string): Array<{x: number, y: number, z: number}>{
+  const childrenArray = scene.querySelectorAll(shape);
   const positionArray: Array<{x: number, y: number, z: number}> = [];
   for (const child of (childrenArray as any)){
     positionArray.push((child as any).components.position.attrValue);
@@ -89,42 +86,24 @@ function getPosition(element: HTMLElement, shape: string): Array<{x: number, y: 
 }
 
 // returns array of each generated objects color
-function getColor(element: HTMLElement, shape: string): Array<string>{
-  const childrenArray = element.querySelectorAll(shape);
-  const attrArray: Array<string> = [];
-  for (const child of (childrenArray as any)){
-    attrArray.push((child as any).getAttribute('color'));
-  }
-  return attrArray;
+function getColor(scene: HTMLElement, shape: string): (string | null)[]{
+  return Array.from(scene.querySelectorAll(shape)).map((point: Element) => point.getAttribute('color'));
 }
 
 // returns array of each generated objects radius
-function getRadius(element: HTMLElement, shape: string): Array<string>{
-  const childrenArray = element.querySelectorAll(shape);
-  const attrArray: Array<string> = [];
-  for (const child of (childrenArray as any)){
-    attrArray.push((child as any).getAttribute('radius'));
-  }
-  return attrArray;
+function getRadius(scene: HTMLElement, shape: string): (string | null)[]{
+  return Array.from(scene.querySelectorAll(shape)).map((point: Element) => point.getAttribute('radius'));
 }
 
 // returns array of each generated objects hover property
-function getHoverable(element: HTMLElement, shape: string): Array<string>{
-  const childrenArray = element.querySelectorAll(shape);
-  const attrArray: Array<string> = [];
-  for (const child of (childrenArray as any)){
-    attrArray.push((child as any).components.hoverable.attrName);
-  }
+function getHoverable(scene: HTMLElement, shape: string): (string | undefined)[]{
+  const attrArray: (string | undefined)[] = [];
+  Array.from(scene.querySelectorAll(shape)).forEach((child) => { attrArray.push((child as Entity).components.hoverable.attrName); });
   return attrArray;
 }
 
 // returns array of each generated objects color, after a hover event has occured
-function getHoveredColor(element: HTMLElement, shape: string): Array<string>{
-  const childrenArray = element.querySelectorAll(shape);
-  const attrArray: Array<string> = [];
-  for (const child of (childrenArray as any)){
-    (child as any).dispatchEvent(new Event('hover-start'));
-    attrArray.push((child as any).getAttribute('color'));
-  }
-  return attrArray;
+function getHoveredColor(scene: HTMLElement, shape: string): (string | null)[]{
+  Array.from(scene.querySelectorAll(shape)).forEach((child) => { child.dispatchEvent(new Event('hover-start')); });
+  return Array.from(scene.querySelectorAll(shape)).map((point: Element) => point.getAttribute('color'));
 }
--- src/d3/hapticplot.d3.ts
+++ src/d3/hapticplot.d3.ts
@@ -12,6 +12,10 @@ export class Hapticplot{
   }
 
   init(container: HTMLElement | null, data: number[]){
+    const POINT_SIZE = 0.05;
+    const DEFAULT_COLOR = 'green';
+    const HOVER_COLOR = 'red';
+
     this.data = data;
     this.container = container;
     // create a scale so that there is correspondence between data set and screen render,
@@ -19,24 +23,26 @@ export class Hapticplot{
     this.hscale = d3.scaleLinear();
     this.hscale.domain([0, d3.max(this.data) as number])  // max of dataset
       .range([0, 100]);
-    this.setupPoints('green', 0.05);
+    this.setupPoints(DEFAULT_COLOR, HOVER_COLOR, POINT_SIZE);
     this.createSky();
     this.createGridPlane();
   }
 
-
+  /**
+   * selects all entities of type datapoint
+   */
   private getShapes(){
-    /*
-    selects all entities of type this.shape
-    */
     return d3.select(this.container).selectAll('datapoint');
   }
 
-  private setupPoints(color, size) {
-    /*
-    Generates points in the scene based on initilization data
-      - represented in scene as this.shape type entities
-    */
+  /**
+   * Generates points in the scene based on initilization data
+   *   - represented in scene as this.shape type entities
+   * @param defaultColor Points default color
+   * @param hoverColor Points color when hovered
+   * @param size Size of each point
+   */
+  private setupPoints(defaultColor, hoverColor, size) {
     this.getShapes()
       // Adds points of type this.shape to the scene
       //  - "enter" identifies any DOM elements to be added when # array
@@ -45,52 +51,57 @@ export class Hapticplot{
       // Updates points positions based on ingested data
       .attr('position', (d, i) =>  this.generatePositions(d, i))
       // Adds given color property to all points
-      .attr('color', color)
+      .attr('color', defaultColor)
       // Sets points radius property
       .attr('radius', size)
       // Enables controller interaction with points using superhands' tags
       .attr('hoverable', '')
       // Adds listeners for state change events, which trigger a change in the
       // point's color property when a hover event occurs
-      .on('hover-start',  (d, i, g) => this.onHoverStart(g[i], d, 'red'))
-      .on('hover-end',  (d, i, g) => this.onHoverEnd(g[i], 'green'));
+      .on('hover-start',  (d, i, g) => this.onHoverStart(g[i], d, hoverColor))
+      .on('hover-end',  (d, i, g) => this.onHoverEnd(g[i], defaultColor));
 
   }
 
+  /**
+   * Generates a world space position for each data entity, based on ingested data
+   * @param data Ingested Data
+   * @param index Crrent index in data array
+   */
   private generatePositions(data, index){
-    /*
-      Generates a world space position for each data entity, based on ingested data
-    */
     const x = index / 10;
     const y = data;
     const z = -1;
     return `${x} ${y} ${z}`;
   }
 
-  private onHoverStart(entity, hapticIntensity, color){
-    /*
-    When an object begins being hovered by the controller entity
-     - triggers a haptic pulse
-     - changes the entities color to indicate a pulse has fired
-    */
-    if (d3.event.detail !== undefined && d3.event.detail.hand !== undefined){
-      d3.event.detail.hand.components.haptics.pulse(hapticIntensity, 1000);
-    }
-    d3.select(entity).attr('color', color);
+  /**
+   * When a data entity begins being hovered by the controller entity
+   *  - triggers a haptic pulse
+   *  - changes the entities color to indicate a pulse has fired
+   * @param entity The entity being hovered
+   * @param hapticIntensity A points haptic intensity, based on is associated data
+   * @param hoverColor The colorthe entity takes on while hoverec
+   */
+  private onHoverStart(entity, hapticIntensity, hoverColor){
+    d3.event.detail?.hand?.components.haptics.pulse(hapticIntensity, 1000);
+    d3.select(entity).attr('color', hoverColor);
   }
 
-  private onHoverEnd(entity, color){
-    /*
-    When an object stops being hovered by the controller entity
-     - changes the entities color to indicate hovering has ended
-    */
-    d3.select(entity).attr('color', color);
+  /**
+   * When an object stops being hovered by the controller entity
+   *  - changes the entities color to indicate hovering has ended
+   * @param entity The data entity which is no longer hovered
+   * @param defaultColor The default color of unhovered entities
+   */
+  private onHoverEnd(entity, defaultColor){
+    d3.select(entity).attr('color', defaultColor);
   }
 
+  /**
+   * creates and adds a sky box to the scene
+   */
   private createSky(){
-    /*
-    creates and adds a sky box to the scene
-    */
     const aSky = document.createElement('a-sky');
     this.container!.appendChild(aSky);
     d3.select(this.container).selectAll('a-sky').attr('color', () => {
@@ -98,11 +109,11 @@ export class Hapticplot{
     });
   }
 
+  /**
+   * creates and adds grid 3D grid lines to the scene
+   */
   private createGridPlane()
   {
-    /*
-    creates and adds grid 3D grid lines to the scene
-    */
     const xGrid = document.createElement('a-entity');
     xGrid.id = 'xGrid';
     this.container!.appendChild(xGrid);
