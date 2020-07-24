3049035445b14e14266a56cd1ebc9e4e0bbef4bb
Jinseo Park <jinseo@google.com>
2020-07-23 22:05:45 -0400

    Use TerritoryLevel as key for the world object

--- src/datasets/geo.dataset.ts
+++ src/datasets/geo.dataset.ts
@@ -5,8 +5,8 @@ import { activeUserMeasure, eventCountMeasure, revenueMeasure } from '../models/
 import { createDefault } from '../utils/preferences';
 import { generateCube } from '../models/data-cube/generation';
 import { Category } from '../models/data-cube/types';
-import { createGeoQuery } from './queries/geo.query';
-import { City, Country, Subcontinent, World } from './geo.types';
+import { createGeoQuery, TerritoryLevel } from './queries/geo.query';
+import { City, Continent, Country, RawWorld, World } from './geo.types';
 import { isNotNullish } from '../utils/misc';
 import * as topojson from 'topojson';
 
@@ -47,7 +47,9 @@ export const configMeta: PreferenceMeta<Config> = {
 };
 
 export async function fetchWorld(): Promise<World> {
-  const world: World = (await import('../assets/world.json')) as any;
+  const world: RawWorld & {
+    continents: Record<string, Continent>
+  } = (await import('../assets/world.json')) as any;
 
   Object.entries(world.continents).forEach(([continentId, continent]) => {
     Object.entries(continent.subcontinents).forEach(([subcontinentId, subcontinent]) => {
@@ -69,37 +71,41 @@ export async function fetchWorld(): Promise<World> {
 
       // merge geometries of subordinate territories
       const countryGeometries = Object.values(subcontinent.countries).map(country => country.geometry).filter(isNotNullish);
-      subcontinent.geometry = countryGeometries.length ? topojson.mergeArcs(world.topology, countryGeometries) : undefined;
+      subcontinent.geometry = countryGeometries.length ? topojson.mergeArcs(world.topology, countryGeometries) : null;
     });
 
     // merge geometries of subordinate territories
     const subcontinentGeometries = Object.values(continent.subcontinents).map(subcontinent => subcontinent.geometry).filter(isNotNullish);
-    continent.geometry = subcontinentGeometries.length ? topojson.mergeArcs(world.topology, subcontinentGeometries) : undefined;
+    continent.geometry = subcontinentGeometries.length ? topojson.mergeArcs(world.topology, subcontinentGeometries) : null;
   });
 
   function unwind<T, K extends keyof T>(parentObject: Record<string, T>, key: K) {
     return Object.values(parentObject).reduce((acc, object) => ({ ...acc, ...object[key] }), {} as T[K]);
   }
 
+  const { continents, topology } = world;
+
   // flatten hierarchical data for each dimension
-  const subcontinents: Record<string, Subcontinent> = unwind(world.continents, 'subcontinents');
+  const subcontinents = unwind(continents, 'subcontinents');
   const countries: Record<string, Country> = unwind(subcontinents, 'countries');
   const cities: Record<string, City> = unwind(countries, 'cities');
 
   return {
-    ...world,
-    subcontinents,
-    countries,
-    cities,
+    [TerritoryLevel.CONTINENT]: continents,
+    [TerritoryLevel.SUBCONTINENT]: subcontinents,
+    [TerritoryLevel.COUNTRY]: countries,
+    [TerritoryLevel.CITY]: cities,
+    topology,
   };
 }
 
 export async function create(config: Config): Promise<Dataset> {
   const world = await fetchWorld();
 
+  const cities = world[TerritoryLevel.CITY];
   const cityCategory: Category = {
     name: 'city',
-    values: Object.entries(world.cities).map(([cityId, city]) => ({
+    values: Object.entries(cities).map(([cityId, city]) => ({
       name: cityId,
       weight: city.population,
     })),
@@ -119,7 +125,7 @@ export async function create(config: Config): Promise<Dataset> {
     createGeoQuery(
       dataCube,
       measures.map(measure => measure.name),
-      world.cities,
+      cities,
     ),
     world,
   );
