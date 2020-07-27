98502229c82a10ff9a6c4ac26df3dd42e1d24721
Chunnien Chan <cnchan@google.com>
2020-07-27 01:06:31 -0700

    Add UserWorkdayHolidayLinearDataset

--- src/datasets/index.ts
+++ src/datasets/index.ts
@@ -2,10 +2,12 @@ import * as UserWhiteNoiseDataset from './user-white-noise.dataset';
 import * as DummyDataset from './dummy.dataset';
 import * as UserWorkdayHolidayDataset from './user-workday-holiday.dataset';
 import * as UserWorkdayHolidayExpDataset from './user-workday-holiday-exp.dataset';
+import * as UserWorkdayHolidayLinearDataset from './user-workday-holiday-linear.dataset';
 
 export const datasets = {
   UserWhiteNoise: UserWhiteNoiseDataset,
   UserWorkdayHoliday: UserWorkdayHolidayDataset,
   UserWorkdayHolidayExp: UserWorkdayHolidayExpDataset,
+  UserWorkdayHolidayLinear: UserWorkdayHolidayLinearDataset,
   Dummy: DummyDataset,
 };
