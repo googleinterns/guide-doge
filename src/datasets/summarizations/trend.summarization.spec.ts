ada205809bde384e363a79532472c92ccd6ae5b3
Chunnien Chan <cnchan@google.com>
2020-08-03 02:52:09 -0700

    Add tests for TrendSummarization

---
+++ src/datasets/summarizations/trend.summarization.spec.ts
@@ -0,0 +1,31 @@
+import { queryFactory } from './trend.summarization';
+import { TimeSeriesPoint } from '../metas/types';
+
+describe('queryFactory', () => {
+  const points: TimeSeriesPoint[] = [];
+  for (let i = 1; i <= 31; i++) {
+    points.push({
+      x: new Date(2020, 6, i),
+      y: i,
+    });
+  }
+  const querySummaries = queryFactory(points);
+  let summaries;
+
+  beforeEach(() => {
+    summaries = querySummaries();
+  });
+
+  it('should return all combinations of linguistic variables.', () => {
+    const nQuantifierOptions = 3;
+    const nTrendDynamicOptions = 5;
+    expect(summaries.length).toBe(nQuantifierOptions * nTrendDynamicOptions);
+  });
+
+  it('should have validity value between 0 and 1.', () => {
+    for (const summary of summaries) {
+      expect(summary.validity).toBeGreaterThanOrEqual(0.0);
+      expect(summary.validity).toBeLessThanOrEqual(1.0);
+    }
+  });
+});
