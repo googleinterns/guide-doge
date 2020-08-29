a4256012cb129f5e11014ec473c2dcacfcba5ef6
Chunnien Chan <cnchan@google.com>
2020-08-28 20:30:32 -0700

    Modify variable name

--- src/datasets/dummy.dataset.ts
+++ src/datasets/dummy.dataset.ts
@@ -22,13 +22,13 @@ export function create(config: Config): Dataset {
   const startDaysBeforeNow = 100;
   const expContinuousGrowthRate = 0.1;
 
-  const time = new Date(Date.now() - startDaysBeforeNow * DAY);
+  const startDate = new Date(Date.now() - startDaysBeforeNow * DAY);
   const data: XYPoint<Date, number>[] = [];
   const rand = random.normal(0, 250);
 
   for (let i = 1; i <= startDaysBeforeNow; i++) {
     data.push({
-      x: new Date(time.getTime() + i * DAY),
+      x: new Date(startDate.getTime() + i * DAY),
       y: Math.exp(i * expContinuousGrowthRate) + config.offset + rand(),
     });
   }
