70bedbd2e282e1dfbdfd9d9e60977e71ac9b7855
Chunnien Chan <cnchan@google.com>
2020-09-13 23:46:21 -0700

    Update tests for TimeSeriesChartD3

--- src/d3/time-series-chart.d3.spec.ts
+++ src/d3/time-series-chart.d3.spec.ts
@@ -17,8 +17,8 @@ interface SubjectRenderOptions extends RenderOptions<TimeSeriesPoint, TestDatum>
 }
 
 describe('TimeSeriesChartD3', () => {
-  // since XYChartD3 is an abstract class, make a concrete child class
-  class TestXYChartD3 extends TimeSeriesChartD3<TestLegendItemStyle> {
+  // since TimeSeriesChartD3 is an abstract class, make a concrete child class
+  class TestTimeSeriesChartD3 extends TimeSeriesChartD3<TestLegendItemStyle> {
     // the flags below will be used to check if the methods have been called at the right time
     dataFlag = 0;
     activePointFlag = 0;
@@ -47,7 +47,7 @@ describe('TimeSeriesChartD3', () => {
   let containerElement: HTMLElement;
   let svgElement: HTMLElement;
   let renderOptions: SubjectRenderOptions;
-  let xyChartD3: TestXYChartD3;
+  let timeSeriesChartD3: TestTimeSeriesChartD3;
 
   beforeEach(() => {
     svgElement = document.createElement('svg');
@@ -60,35 +60,35 @@ describe('TimeSeriesChartD3', () => {
       data$: new Subject<LineChartDatum[]>(),
       activePoint$: new Subject<TimeSeriesPoint | null>(),
     };
-    xyChartD3 = new TestXYChartD3(renderOptions);
+    timeSeriesChartD3 = new TestTimeSeriesChartD3(renderOptions);
   });
 
   afterEach(() => {
-    xyChartD3.clear();
+    timeSeriesChartD3.clear();
   });
 
   it('should instantiate.', () => {
-    expect(xyChartD3).toBeInstanceOf(XYChartD3);
+    expect(timeSeriesChartD3).toBeInstanceOf(XYChartD3);
   });
 
   it('should render the data and update upon changes.', () => {
-    expect(xyChartD3.dataFlag).toBe(0);
-    xyChartD3.render();
-    expect(xyChartD3.dataFlag).toBe(1);
+    expect(timeSeriesChartD3.dataFlag).toBe(0);
+    timeSeriesChartD3.render();
+    expect(timeSeriesChartD3.dataFlag).toBe(1);
     renderOptions.data$.next(mockData);
-    expect(xyChartD3.dataFlag).toBe(2);
+    expect(timeSeriesChartD3.dataFlag).toBe(2);
   });
 
   it('should render the active datum and update upon changes.', () => {
-    expect(xyChartD3.activePointFlag).toBe(0);
-    xyChartD3.render();
-    expect(xyChartD3.activePointFlag).toBe(1);
+    expect(timeSeriesChartD3.activePointFlag).toBe(0);
+    timeSeriesChartD3.render();
+    expect(timeSeriesChartD3.activePointFlag).toBe(1);
     renderOptions.activePoint$.next(null);
-    expect(xyChartD3.activePointFlag).toBe(2);
+    expect(timeSeriesChartD3.activePointFlag).toBe(2);
   });
 
   it('should render the x and y axis.', () => {
-    xyChartD3.render();
+    timeSeriesChartD3.render();
     const xAxisElement = svgElement.querySelector('.xy_chart-x_axis');
     const yAxisElement = svgElement.querySelector('.xy_chart-y_axis');
     expect(xAxisElement).not.toBe(null);
@@ -96,7 +96,7 @@ describe('TimeSeriesChartD3', () => {
   });
 
   it('should render the legend.', () => {
-    xyChartD3.render();
+    timeSeriesChartD3.render();
     renderOptions.data$.next(mockData);
     const textElements = svgElement.querySelectorAll('.xy_chart-legend text');
     expect(textElements.length).toBe(mockData.length);
