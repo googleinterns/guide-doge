7bc14fc39796628dff89ca6f2162b12a9988a039
Chunnien Chan <cnchan@google.com>
2020-09-16 20:13:26 -0700

    Add tests for SummarizationControlService

---
+++ src/services/summarization/summarization-control.service.spec.ts
@@ -0,0 +1,125 @@
+import { Observable, of } from 'rxjs';
+import { SUMMARIZATION, SummaryGroup } from './types';
+import { SummarizationControlService } from './summarization-control.service';
+import { SummarizationService, BaseConfig } from './summarization.service';
+import { WeekdayWeekendRelativeSummarizationService } from './weekday-weekend-relative.summarization.service';
+import { TrendRegressionSummarizationService } from './trend-regression.summarization.service';
+import { TrendPartialSummarizationService } from './trend-partial.summarization.service';
+import { TrendWeeklyComparisonAverageSummarizationService } from './trend-weekly-comparison-average.summarization.service';
+import { TrendWeeklyComparisonRateSummarizationService } from './trend-weekly-comparison-rate.summarization.service';
+import { TrendWeeklyElaborationSummarizationService } from './trend-weekly-elaboration.summarization.service';
+import { TrendWeeklyPatternSummarizationService } from './trend-weekly-pattern.summarization.service';
+import { TimeSeriesPoint } from '../../datasets/metas/types';
+
+
+// Just need to implement `summaries$` for testing `SummarizationControlService`.
+class TestSummarizationService {
+  summaries$(): Observable<SummaryGroup[]> { return of([]); }
+}
+
+describe('queryFactory', () => {
+  let summarizationControlService: SummarizationControlService;
+
+  let weekdayWeekendRelativeSummarizationService: WeekdayWeekendRelativeSummarizationService;
+  let trendRegressionSummarizationService: TrendRegressionSummarizationService;
+  let trendPartialSummarizationService: TrendPartialSummarizationService;
+  let trendWeeklyComparisonAverageSummarizationService: TrendWeeklyComparisonAverageSummarizationService;
+  let trendWeeklyComparisonRateSummarizationService: TrendWeeklyComparisonRateSummarizationService;
+  let trendWeeklyElaborationSummarizationService: TrendWeeklyElaborationSummarizationService;
+  let trendWeeklyPatternSummarizationService: TrendWeeklyPatternSummarizationService;
+
+  beforeEach(() => {
+    weekdayWeekendRelativeSummarizationService = new TestSummarizationService() as any;
+    trendRegressionSummarizationService = new TestSummarizationService() as any;
+    trendPartialSummarizationService = new TestSummarizationService() as any;
+    trendWeeklyComparisonAverageSummarizationService = new TestSummarizationService() as any;
+    trendWeeklyComparisonRateSummarizationService = new TestSummarizationService() as any;
+    trendWeeklyElaborationSummarizationService = new TestSummarizationService() as any;
+    trendWeeklyPatternSummarizationService = new TestSummarizationService() as any;
+
+    summarizationControlService = new SummarizationControlService(
+      weekdayWeekendRelativeSummarizationService,
+      trendRegressionSummarizationService,
+      trendPartialSummarizationService,
+      trendWeeklyComparisonAverageSummarizationService,
+      trendWeeklyComparisonRateSummarizationService,
+      trendWeeklyElaborationSummarizationService,
+      trendWeeklyPatternSummarizationService,
+    );
+  });
+
+  it('should instantiate.', () => {
+    expect(summarizationControlService).toBeInstanceOf(SummarizationControlService);
+  });
+
+  it('should return summary groups from WeekdayWeekendRelativeSummarizationService.', () => {
+    const summarization = SUMMARIZATION.WEEKDAY_WEEKEND_RELATIVE;
+    const summarizationMetas = [{ summarization, config: { datumLabels: [] } }];
+
+    spyOn(weekdayWeekendRelativeSummarizationService, 'summaries$');
+
+    summarizationControlService.summaries$(summarizationMetas);
+    expect(weekdayWeekendRelativeSummarizationService.summaries$).toHaveBeenCalled();
+  });
+
+  it('should return summary groups from TrendRegressionSummarizationService.', () => {
+    const summarization = SUMMARIZATION.TREND_REGRESSION;
+    const summarizationMetas = [{ summarization, config: { datumLabels: [] } }];
+
+    spyOn(trendRegressionSummarizationService, 'summaries$');
+
+    summarizationControlService.summaries$(summarizationMetas);
+    expect(trendRegressionSummarizationService.summaries$).toHaveBeenCalled();
+  });
+
+  it('should return summary groups from TrendPartialSummarizationService.', () => {
+    const summarization = SUMMARIZATION.TREND_PARTIAL;
+    const summarizationMetas = [{ summarization, config: { datumLabels: [] } }];
+
+    spyOn(trendPartialSummarizationService, 'summaries$');
+
+    summarizationControlService.summaries$(summarizationMetas);
+    expect(trendPartialSummarizationService.summaries$).toHaveBeenCalled();
+  });
+
+  it('should return summary groups from TrendWeeklyComparisonAverageSummarizationService.', () => {
+    const summarization = SUMMARIZATION.TREND_WEEKLY_COMPARISON_AVERAGE;
+    const summarizationMetas = [{ summarization, config: { datumLabels: [] } }];
+
+    spyOn(trendWeeklyComparisonAverageSummarizationService, 'summaries$');
+
+    summarizationControlService.summaries$(summarizationMetas);
+    expect(trendWeeklyComparisonAverageSummarizationService.summaries$).toHaveBeenCalled();
+  });
+
+  it('should return summary groups from TrendWeeklyComparisonRateSummarizationService.', () => {
+    const summarization = SUMMARIZATION.TREND_WEEKLY_COMPARISON_RATE;
+    const summarizationMetas = [{ summarization, config: { datumLabels: [] } }];
+
+    spyOn(trendWeeklyComparisonRateSummarizationService, 'summaries$');
+
+    summarizationControlService.summaries$(summarizationMetas);
+    expect(trendWeeklyComparisonRateSummarizationService.summaries$).toHaveBeenCalled();
+  });
+
+  it('should return summary groups from TrendWeeklyElaborationSummarizationService.', () => {
+    const summarization = SUMMARIZATION.TREND_WEEKLY_ELABORATION;
+    const summarizationMetas = [{ summarization, config: { datumLabels: [] } }];
+
+    spyOn(trendWeeklyElaborationSummarizationService, 'summaries$');
+
+    summarizationControlService.summaries$(summarizationMetas);
+    expect(trendWeeklyElaborationSummarizationService.summaries$).toHaveBeenCalled();
+  });
+
+  it('should return summary groups from TrendWeeklyPatternSummarizationService.', () => {
+    const summarization = SUMMARIZATION.TREND_WEEKLY_PATTERN;
+    const summarizationMetas = [{ summarization, config: { datumLabels: [] } }];
+
+    spyOn(trendWeeklyPatternSummarizationService, 'summaries$');
+
+    summarizationControlService.summaries$(summarizationMetas);
+    expect(trendWeeklyPatternSummarizationService.summaries$).toHaveBeenCalled();
+  });
+});
+
